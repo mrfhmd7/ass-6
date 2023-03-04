bca51d2fff00963744496a423b3c7c19f4c6ea88
mrfhmd7 <mrf.hmd7@gmail.com>
2023-03-04 21:53:43 +0600

    add div to container

--- js/index.js
+++ js/index.js
@@ -1,1 +1,55 @@
-console.log('index');
+const loadPosts= (dataLimit) => {
+    toggleSpinner(true);
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+      .then((res) => res.json())
+      .then((data) => displayAI(data.data.tools,dataLimit));
+      
+  }
+  
+  const displayAI = (AI,dataLimit) =>{
+    const aiContainer = document.getElementById('AI-container');
+    aiContainer.innerHTML = '';
+    //console.log(AI);
+  
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && AI.length > 6){
+      AI = AI.slice(0,6);
+      showAll.classList.remove('d-none');
+    }
+    else{
+      showAll.classList.add('d-none');
+    }
+    AI.forEach(Ai => {
+        const div = document.createElement('div');
+        div.classList.add('col') ;
+        div.innerHTML = `
+        <div class="card p-2 h-100">
+                <img src="${Ai.image}" class="card-img-top img-fluid" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Features</h5>
+            <ol type="1">
+              <li>${Ai.features[0]}</li>
+              <li>${Ai.features[1]}</li>
+              <li>${Ai.features[2]}</li>
+            </ol>
+              </div>
+              <hr>
+              <div class="d-flex justify-content-between">
+                <div>
+                  <h5>${Ai.name}</h5>
+                  <p><i class="fa-regular fa-calendar-days"></i> ${Ai.published_in}</p>
+                </div>
+                <button onclick="aiDetails('${Ai.id}')" id="details-arrow" class="rounded-circle my-5 my-auto border bg-white border-0" data-bs-toggle="modal" data-bs-target="#aiModal"><i class="fa-solid fa-arrow-right text-danger fs-5"></i></button>
+  
+                
+              </div>
+          </div>
+        ` 
+        aiContainer.appendChild(div);
+         toggleSpinner(false);
+    });
+    
+  }
+
+
+  loadPosts(7);
