33c870477b939bbc17d939c447b3621e7268b17a
mrfhmd7 <mrf.hmd7@gmail.com>
2023-03-04 22:14:47 +0600

    add api tools

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,8 @@
-console.log('modal');
+const loadPosts= (dataLimit) => {
+    toggleSpinner(true);
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+      .then((res) => res.json())
+      .then((data) => displayAI(data.data.tools,dataLimit));
+      
+  }
+  
